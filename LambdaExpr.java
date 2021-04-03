98e0e5346755c8c9814186a8fba6cbd782866624
sidkulk <kulksud@gmail.com>
2021-04-03 19:33:29 +0530

    added block Lambda expression code

--- LambdaExpr.java
+++ LambdaExpr.java
@@ -35,9 +35,75 @@ import static java.lang.System.out;
 */
 
 
+//A functional interface with only ONE abstract method..
+interface MyInterface {
+	double getValue();
+}
+
+interface Calculator {
+	double getValue(double x, double y);
+}
+
+interface EvenOdd {
+	boolean isEven(int ob);
+}
 
 class Main {
 	public static void main(String[] args) {
+		MyInterface myinter = () -> Math.random() * 100;
 		
+		Calculator addOper = (x, y) -> (x + y);
+		Calculator subOper = (x, y) -> (x - y);
+		Calculator mulOper = (x, y) -> (x * y);
+		Calculator divOper = (x, y) -> (x / y);
+		
+		System.out.println(myinter.getValue());
+		System.out.println("addition of 1 and 1: "+addOper.getValue(1, 1));
+		System.out.println("subtraction of 2 and 1: "+subOper.getValue(2, 1));
+		System.out.println("multiplication of 3 and 5: "+mulOper.getValue(3, 5));
+		
+		try {
+			double res = divOper.getValue(1, 0);
+			System.out.println("division of 1 and 0: "+res);
+		} catch(Exception e) {
+			System.out.println("Exception occured: "+e);
+			e.printStackTrace();
+		}
+		
+		EvenOdd iObj = (n) -> (n%2==0);
+		out.println("is 2 an even number?: "+iObj.isEven(2));
+		out.println("is 3 an even number?: "+iObj.isEven(3));
+		
+	}
+}
+
+//Block Lambda Expressions: 
+
+/**
+ * 	These are very much the same compared to Expression bodies that we 
+ * 	just talked about in the previous section.
+ * 
+ * 	Block Lambda expressions are enclosed in curly braces and explicitely 
+ * 	some value. So they must contain a return statement.
+ **/
+
+interface NumFun {
+	int getResult(int n); //A functional interface
+}
+
+class BlockLambDemp {
+	public static void main(String[] args) {
+		NumFun num = (n) -> {
+			
+			int res = 1;
+			for(int i=1;i<=n;i++) {
+				res = res*i;
+			}
+			
+			return res;
+		};
+		
+		out.println("The factorial of of 2 is: "+num.getResult(2));
+		out.println("The factorial of of 10 is: "+num.getResult(10));
 	}
 }
